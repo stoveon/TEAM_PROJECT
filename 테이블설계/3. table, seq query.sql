623b2ba353adcd8ac0154b66be11f96b9a0341de
Rachel-jieun Jeong <84854563+stoveon@users.noreply.github.com>
2021-10-20 16:54:00 +0900

    테이블설계(6차)
    회원, 상품 테이블 수정

--- 테이블설계/3. table, seq query.sql
+++ 테이블설계/3. table, seq query.sql
@@ -1,31 +1,33 @@
 create table MEMBER(
 NUM number primary key,
 EMAIL varchar2(50) not null unique,
+NICKNAME varchar2(20) not null unique,
 PASSWORD varchar2(50) not null,
 NAME varchar2(20) not null,
-NICKNAME varchar2(20) not null unique,
-WARNCOUNT number default 0,
-SESSIONID varchar2(100),
-OUTDATE date
+GENDER varchar2(10),
+STOREDATE date
 );
 
-create table MEMBER_PLUS(
+
+create table MEMBER_AUTO(
 NUM number primary key,
 NICKNAME varchar2(20) not null unique,
-GENDER varchar2(10),
-INDATE date default sysdate,
-STOREDATE date
+WARNCOUNT number default 0,
+POINT number default 0,
+REGDATE date,
+MODDATE date default sysdate,
+SESSIONID varchar2(100),
+LOGINTIME date
 );
 
 create table MEMBER_ADDR(
 NUM number primary key,
 NICKNAME varchar2(20) not null unique,
 PHONE1 varchar2(10),
-PHONE2 varchar2(10),
+PHONE2 varchar2(20),
 ADDRNUM varchar2(10),
 ADDR varchar2(100),
-ADDRSUB varchar2(100),
-POINT number default 0
+ADDRSUB varchar2(100)
 );
 
 create table POINT(
@@ -84,8 +86,6 @@ HEART varchar2(10) default 'NO' not null,
 STOCK number default 20
 );
 
-
-
 create table GOODS_IMAGE(
 NUM number primary key,
 GOODSCODE varchar2(100) not null,
@@ -96,9 +96,10 @@ create table GOODS_SALES(
 NUM number primary key,
 GOODSCODE varchar2(100) not null,
 NICKNAME varchar2(20) not null,
-SALES number not null,
+PRICE number not null,
 ORDERDATE date default sysdate
 );
+
 
 create table ASSATALK(
 NUM number primary key,
@@ -140,7 +141,7 @@ create sequence MEMBER_SEQ
 start with 1 
 increment by 1 
 nomaxvalue nocycle nocache noorder;
-create sequence MEMBER_PLUS_SEQ
+create sequence MEMBER_AUTO_SEQ
 start with 1 
 increment by 1 
 nomaxvalue nocycle nocache noorder;
