61d5b44c3fa0eaa046bd49d68ae54df7251e52de
Rachel-jieun Jeong <84854563+stoveon@users.noreply.github.com>
2021-10-25 00:10:10 +0900

    Delete insidehome directory

--- insidehome/src/main/java/home/inside/goods/repository/IGoodsSalesDao.java
+++
@@ -1,8 +0,0 @@
-package home.inside.goods.repository;
-
-import home.inside.goods.vo.GoodsSalesVo;
-
-public interface IGoodsSalesDao {
-	public void insertGoodsSales(GoodsSalesVo goodsSalesVo) throws Exception;
-	public void deleteGoodsSales(String nickname) throws Exception;
-}
