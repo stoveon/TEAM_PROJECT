874e7c7d83edb8a18b45d544c08630e2225aa1bb
Rachel-jieun Jeong <84854563+stoveon@users.noreply.github.com>
2021-11-04 00:22:55 +0900

    goods_sales 테이블 및 더미 수정 (배송날짜 컬럼 추가)

--- 구현/쿼리문/goods_sales.sql
+++ 구현/쿼리문/goods_sales.sql
@@ -5,9 +5,10 @@ create table GOODS_SALES(
 NUM number primary key,
 GOODSCODE varchar2(100) not null,
 NICKNAME varchar2(20) not null,
-SALES number not null,
+PRICE number not null,
 ORDERDATE date default sysdate,
-SENDSTATE varchar2(10) default 'NO' not null
+SENDSTATE varchar2(10) default 'YET' not null
+SENDDATE date,
 );
 
 create sequence GOODS_SALES_SEQ
@@ -15,13 +16,13 @@ start with 1
 increment by 1 
 nomaxvalue nocycle nocache noorder;
 
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test1', 'eun_naver', 25900, sysdate, 'END');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test2', 'jin_naver', 18900, sysdate, 'END');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test3', 'seoul_naver', 7800, sysdate, 'YET');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test4', 'sun_naver', 12800, sysdate, 'YET');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test5', 'hee_naver', 17900, sysdate, 'ING');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test6', 'eun_naver', 13900, sysdate, 'YET');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test7', 'jin_naver', 6000, sysdate, 'ING');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test6', 'seoul_inside', 13900, sysdate, 'YET');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test5', 'hee_inside', 17900, sysdate, 'YET');
-insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test4', 'sun_inside', 12800, sysdate, 'YET');
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test1', 'eun_naver', 25900, sysdate, 'END' ,sysdate+1);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test2', 'jin_naver', 18900, sysdate, 'END' ,sysdate+1);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test3', 'seoul_naver', 7800, sysdate, 'YET' ,sysdate+6);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test4', 'sun_naver', 12800, sysdate, 'YET' ,sysdate+1);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test5', 'hee_naver', 17900, sysdate, 'ING' ,sysdate+1);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test6', 'eun_naver', 13900, sysdate, 'YET' ,sysdate+4);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test7', 'jin_naver', 6000, sysdate, 'ING' ,sysdate+2);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test6', 'seoul_inside', 13900, sysdate, 'YET' ,sysdate+3);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test5', 'hee_inside', 17900, sysdate, 'YET' ,sysdate-+7);
+insert into GOODS_SALES values (GOODS_SALES_SEQ.nextval, 'test4', 'sun_inside', 12800, sysdate, 'YET' ,sysdate+5);
